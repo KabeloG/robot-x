a8fef36adc6dcd5835f0dd7ea4ee0ba14a5f9c12
KabeloG <kabelogumani@gmail.com>
2024-01-23 16:35:06 +0200

    package install

--- src/app/trading-bots/page.tsx
+++ src/app/trading-bots/page.tsx
@@ -1,7 +1,7 @@
 // import trading chartData
 import { candlesDataTwo, volumeData } from '@/data/static/trading-data';
 
-import MinimalLayout from '@/layouts/minimal/layout';
+import ModernLayout from '@/layouts/modern/layout';
 import BotFilterTab from '@/components/trading-bot/bot-filter-tab';
 import CoinBar from '@/components/trading-bot/coin-bar';
 import CoinList from '@/components/trading-bot/coin-list';
@@ -11,7 +11,7 @@ import InvestmentTab from '@/components/trading-bot/investment-tab';
 
 export default function TradingBot() {
   return (
-    <MinimalLayout>
+    <ModernLayout>
       <BotFilterTab />
       <CoinBar />
       <div className="mt-4 grid grid-cols-12 gap-6 @container">
@@ -26,6 +26,6 @@ export default function TradingBot() {
         </div>
       </div>
       <InvestmentTab />
-    </MinimalLayout>
+    </ModernLayout>
   );
 }
