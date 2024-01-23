a332a044c4da4690643a1bee7586fdb12e1bf78c
KabeloG <kabelogumani@gmail.com>
2024-01-23 16:27:18 +0200

    first commit

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.coingecko.com',
+      },
+    ],
+  },
+  ...(process.env.NODE_ENV === 'production' && {
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+  }),
+};
+
+module.exports = nextConfig;
